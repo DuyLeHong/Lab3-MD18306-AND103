10b59305bc2bd2da945d58c25927cdd8816bead6
Duy Le <lehongduy.bk@gmail.com>
2025-03-27 14:46:50 +0700

    update code SP25

--- COMMON.js
+++ COMMON.js
@@ -1,4 +1,4 @@
-const uri = 'mongodb+srv://admin5:2crp8olmWEwv9PFB@cluster0.0n8qgpd.mongodb.net/md18306';
+const uri = 'mongodb+srv://md20101:EQSbfcnDBwrKTPVc@cluster0.0n8qgpd.mongodb.net/md18306';
 
 module.exports = {
     uri: uri,
--- api.js
+++ api.js
@@ -24,3 +24,26 @@ router.get('/list', async (req, res) => {
 
     res.send(cars);
 })
+
+router.post('/add_xe', async (req, res) => {
+    await mongoose.connect(COMMON.uri);
+
+    // let car = {
+    //     ten: 'xe 3',
+    //     namSX: 2024,
+    //     hang: 'Mitsubishi',
+    //     gia: 7500
+    // }
+
+    let car = req.body;
+
+    console.log(car)
+
+    let kq = await carModel.create(car);
+    console.log(kq);
+
+    let cars = await carModel.find();
+
+    res.send(cars);
+
+})
